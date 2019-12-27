3c9c00ca5715033b96daa7cd83fac2336780e06d
Josh Herman <jherman4@vols.utk.edu>
2019-12-27 11:49:26 -0500

    removed the extra lines from the OT parser.

--- OT_parser.py
+++ OT_parser.py
@@ -1,6 +1,5 @@
 import os
 from os.path import isfile, join
-import csv
 import GlobalSettings
 from CSPRparser import CSPRparser
 
@@ -16,8 +15,6 @@ class ot_parser():
 
         self.offdict = dict()
         self.chromdict = dict()
-        self.off_temp = list()
-        self.chrom_temp = list()
 
     """
         get_data: this function loads the data in from the temporary OT file 
@@ -30,8 +27,8 @@ class ot_parser():
     def get_data(self, ot_data_path, cspr_file_path):
         self.offdict.clear()
         self.chromdict.clear()
-        self.chrom_temp.clear()
-        self.off_temp.clear()
+        chrom_temp = list()
+        off_temp = list()
         fp = open(ot_data_path)
 
         myList = fp.read().splitlines()
@@ -45,15 +42,15 @@ class ot_parser():
                     self.offdict[y[0]] = y[1]
                 else:
                     temp_seq = y[0]
-                    self.off_temp = []
-                    self.chrom_temp = []
+                    off_temp = []
+                    chrom_temp = []
             elif x[0] == '0':
                 line_temp = [e.strip() for e in x.split(',')]
-                self.off_temp.append((line_temp[0]))
-                self.chrom_temp.append(line_temp[1])
-                self.offdict[temp_seq] = self.off_temp
-                self.chromdict[temp_seq] = self.chrom_temp
-        
+                off_temp.append((line_temp[0]))
+                chrom_temp.append(line_temp[1])
+                self.offdict[temp_seq] = off_temp
+                self.chromdict[temp_seq] = chrom_temp
+
         self.write_data(cspr_file_path)
 
     """
@@ -67,18 +64,17 @@ class ot_parser():
         ref_list = parser.get_orgs()
         
         with open(self.appended_file, 'w+') as data_output:
-            data_writer = csv.writer(data_output, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            data_output.write("Sequence,Off-Target Score,Organism\n")
             for key, values in self.offdict.items():
-                print(values)
                 # just output the sequence and the 0 if no hits
                 if float(values[0]) == 0.0000000:
-                    data_writer.writerow([key, values])
-                # if there are hits, output the organism as well
+                    data_output.write(str(key) + ',' + str(values) + '\n')
+                # if there are hits, output the organism as well, as well as every hit
                 else:
                     chrom_values = self.chromdict.get(key)
                     chrom_values = self.update_chrom_values(ref_list, chrom_values)
                     for i in range(len(values)):
-                        data_writer.writerow([key, values[i], chrom_values[i]])
+                        data_output.write(str(key) + ',' + str(values[i]) + ',' + str(chrom_values[i]) + '\n')
 
     """
         update_chrom_values: this function takes the chromosome index and 
@@ -98,58 +94,3 @@ class ot_parser():
         for i in chrom_values:
             ret_list.append(str(ref_list[int(i)-1][0]))
         return ret_list
-
-
-
-"""
-## Append all off-target .txt files into one .txt file:
-## Path to off-target .txt files is hard-coded.
-path = "/home/ddooley/phylo_vip/data/ATCC_off/sao ATCC Results/"
-file_list = [f for f in os.listdir(path) if isfile(join(path, f))]
-nt = ['A', 'C', 'G', 'T']
-appended_file = input("Please give an appended/output file name (without extension): ")
-with open(os.getcwd() + "/" + appended_file + '.txt', 'w+') as outfile:
-    for fname in file_list:
-        infile = open(path + fname, "r")
-        outfile.write(infile.read() + '\n')
-        ind = file_list.index(fname)
-        if ind == len(file_list)-1:
-            outfile.write(infile.read() + '\n' + 'END OF FILE')
-
-## Look through off-target data and make two dictionaries, each with target sequences as key,
-## one with all off-target scores and one with all off-target hit locations
-f = open(os.getcwd() + "/" + appended_file + '.txt', 'r')
-offdict = {}
-chromdict = {}
-off_temp = []
-chrom_temp = []
-mylist = f.read().splitlines()
-good_lines = [line for line in mylist if line]
-for x in good_lines:
-    if x[0] in nt:
-        if x[21:29] == '0.000000':
-            offdict[x[0:19]] = x[21:28]
-        else:
-            temp_seq = x[0:19]
-            off_temp = []
-            chrom_temp = []
-    elif x[0] == '0':
-        line_temp = [e.strip() for e in x.split(',')]
-        off_temp.append((line_temp[0]))
-        chrom_temp.append(line_temp[1])
-        offdict[temp_seq] = off_temp
-        chromdict[temp_seq] = chrom_temp
-f.close()
-
-
-## Write off-target data to CSV file
-with open(os.getcwd() +"/" + appended_file + '.csv', 'w+') as data_output:
-    data_writer = csv.writer(data_output, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for key, values in offdict.items():
-        if float(values[0]) == 0.00000:
-            data_writer.writerow([key, values])
-        else:
-            chrom_values = chromdict.get(key)
-            for i in range(len(values)):
-                data_writer.writerow([key, values[i], chrom_values[i]])
-"""
